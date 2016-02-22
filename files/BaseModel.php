07919fc56e630fc7c5479246436258a076c349f5
jellingsen <nullsjuhundre@gmail.com>
2016-02-22 15:15:09 +0100

    Added optional setFields() function for improved performance

--- files/BaseModel.php
+++ files/BaseModel.php
@@ -11,13 +11,12 @@ require('QueryBuilder.php');
 class BaseModel
 {
 	protected $wpdb;
-	private $fieldArray;
+	public $fieldArray;
 	public $primaryKey;
 
 	protected function __construct() {
 		global $wpdb;
 		$this->wpdb = &$wpdb;
-		$this->getFields();
 	}
 
 	/**
@@ -49,7 +48,9 @@ class BaseModel
 	{
 		if($this->fieldArray == null)
 		{
+			$start = microtime(true);
 			$results = $this->wpdb->get_results('SHOW columns FROM '.$this->getTable());
+			$GLOBALS['DB_MODELS_DEBUG'][] = ['SHOW columns FROM '.$this->getTable(), microtime(true) - $start, false];
 			foreach($results as $field)
 			{
 				if(strtolower($field->Key) == 'pri') $this->primaryKey = $field->Field;
@@ -67,6 +68,7 @@ class BaseModel
 	public static function find()
 	{
 		$instance = self::createInstance();
+		$instance->getFields();
 		return new QueryBuilder($instance->getTable(), 'SELECT');
 	}
 
@@ -80,6 +82,7 @@ class BaseModel
 	public static function findByPk($pk, $select = null)
 	{
 		$instance = self::createInstance();
+		$instance->getFields();
 		if($instance->primaryKey == null) return 'nopk';
 		$query = new QueryBuilder($instance->getTable(), 'SELECT');
 		if($select != null) $query->select($select);
@@ -90,5 +93,12 @@ class BaseModel
 	{
 		$class = get_called_class();
 		return new $class;
+	}
+
+	protected function setFields($priKey, $fields)
+	{
+		$this->primaryKey = $priKey;
+		array_unshift($fields, $priKey);
+		$this->fieldArray = $fields;
 	}
 }
--- models/Options.php
+++ models/Options.php
@@ -1,10 +1,8 @@
 <?php
 
 /**
- * Created by PhpStorm.
- * User: e
- * Date: 16-Feb-16
- * Time: 18:59
+ * Example Model
+ * Copy this base code for all your tables, and update tableName()
  */
 require('/../files' . DIRECTORY_SEPARATOR. 'BaseModel.php');
 
@@ -13,13 +11,19 @@ class Options extends BaseModel
 	public function __construct()
 	{
 		parent::__construct();
+
+		// Optional performance enhancing function $this->setFields(primary_key, [attributes])
+		$this->setFields('options_id', ['option_name','option_value','autoload']);
 	}
 
-	/*
+	/**
 	 * Database Table name (Prefix is added automatically!)
 	 */
 	static function tableName()
 	{
 		return 'options';
 	}
+
+
+
 }
